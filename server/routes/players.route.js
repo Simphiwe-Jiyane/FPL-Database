8b99db6d4e49e9350cecdb604bf6995945765f55
Simphiwe Jiyane <jason.jiyane@gmail.com>
2021-12-21 11:49:33 +0200

    Changed return player

--- server/routes/players.route.js
+++ server/routes/players.route.js
@@ -4,9 +4,9 @@ const router = express.Router();
 router.get('/getPlayer', (req, res, next) => {
     res.json({ success: true ,player: {
         id: 1,
-        firstname: 'Simphiwe',
+        firstname: 'Sanele',
         lastname: 'Jiyane',
-        team: 'Kaizer Chiefs'
+        team: 'Mamelodi Sundowns'
     }})
 });
 
